34d9d5330b3c12b8b50b350f7217b6971d6a23f3
victorpereiira <victorrpsantos@gmail.com>
2023-03-16 23:30:42 -0300

    start project

---
+++ application/models/games_model.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Games_model extends CI_Model {
+    public function index()
+    {
+        return $this->db->get("tb_games")->result_array();
+    }
+}
